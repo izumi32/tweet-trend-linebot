d7e211414c36539bc0a229de94162f28a3f08118
izumi32 <k.izumi721@gmail.com>
2021-11-29 19:35:40 +0900

    reply trend

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -4,21 +4,21 @@ class LineBotController < ApplicationController
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
+    unless line_client.validate_signature(body, signature)
       return head :bad_request
     end
 
-    events = client.parse_events_from(body)
+    events = line_client.parse_events_from(body)
     events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+          line_client.reply_message(event['replyToken'], create_message)
         end
       end
     end
@@ -27,10 +27,32 @@ class LineBotController < ApplicationController
 
   private
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
+  def line_client
+    @line_client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+
+  def twitter_client
+    @twitter_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_API_KEY']
+      config.consumer_secret = ENV['TWITTER_API_SECRET_KEY']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET_TOKEN']
+    end
+  end
+
+  def create_message
+    trends = twitter_client.trends(id = 23424856).attrs[:trends].first(10)
+    text = ''
+    trends.each do |trend|
+      text << trend[:name] + "\n" + trend[:url] + "\n" + "\n"
+    end
+
+    message = {
+      type: 'text',
+      text: text
     }
   end
 end
