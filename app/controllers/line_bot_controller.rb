46690e71ec54f12db3bb867e31850400043aa1f9
izumi32 <k.izumi721@gmail.com>
2021-12-02 22:37:16 +0900

    reply機能をコメントアウト

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -11,11 +11,11 @@ class LineBotController < ApplicationController
     events = line_client.parse_events_from(body)
     events.each do |event|
       case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          line_client.reply_message(event['replyToken'], create_message)
-        end
+      # when Line::Bot::Event::Message
+      #   case event.type
+      #   when Line::Bot::Event::MessageType::Text
+      #     line_client.reply_message(event['replyToken'], create_message)
+      #   end
       when Line::Bot::Event::Follow
         line_id = event['source']['userId']
         User.create(line_id: line_id)
@@ -36,25 +36,25 @@ class LineBotController < ApplicationController
     end
   end
 
-  def twitter_client
-    @twitter_client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['TWITTER_API_KEY']
-      config.consumer_secret = ENV['TWITTER_API_SECRET_KEY']
-      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET_TOKEN']
-    end
-  end
+  # def twitter_client
+  #   @twitter_client ||= Twitter::REST::Client.new do |config|
+  #     config.consumer_key = ENV['TWITTER_API_KEY']
+  #     config.consumer_secret = ENV['TWITTER_API_SECRET_KEY']
+  #     config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+  #     config.access_token_secret = ENV['TWITTER_ACCESS_SECRET_TOKEN']
+  #   end
+  # end
 
-  def create_message
-    trends = twitter_client.trends(id = 23424856).attrs[:trends].first(10)
-    text = ''
-    trends.each do |trend|
-      text << trend[:name] + "\n" + trend[:url] + "\n" + "\n"
-    end
-
-    message = {
-      type: 'text',
-      text: text
-    }
-  end
+  # def create_message
+  #   trends = twitter_client.trends(id = 23424856).attrs[:trends].first(10)
+  #   text = ''
+  #   trends.each do |trend|
+  #     text << trend[:name] + "\n" + trend[:url] + "\n" + "\n"
+  #   end
+  #
+  #   message = {
+  #     type: 'text',
+  #     text: text
+  #   }
+  # end
 end
